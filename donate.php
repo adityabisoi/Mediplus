f833fca1ba9e618ec2045e16f63d696b8d66af44
adityabisoi <adityabisoi1999@gmail.com>
2020-01-29 00:09:57 +0530

    blood donation system added

---
+++ donate.php
@@ -0,0 +1,117 @@
+<!--
+Author: Colorlib
+Author URL: https://colorlib.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+<!-- //web font -->
+</head>
+<body>
+
+    <?php
+
+        $na=$em=$ty="";
+        $status=true;
+        if(!empty($_POST))
+            {
+                if(!empty($_POST["name"]))
+                    {
+                        $na=$_POST["name"];
+                    }
+                else{
+                    $status=false;
+                }
+        
+            
+                if(!empty($_POST["email"]))
+                    {
+                        $em=$_POST["email"];
+                    }
+                else{
+                    $status=false;
+                }
+                if(!empty($_POST["type"]))
+                    {
+                        $ty=$_POST["type"];
+                    }
+                else{
+                    $status=false;
+                }
+
+        //db connection
+
+        $servername="localhost";
+        $username="root";
+        $password="";
+        $dbname="electrothon";
+
+        $conn=new mysqli($servername,$username,$password,$dbname);
+        if($conn->connect_error)
+        {
+            die("Connection failed".$conn->connect_error);
+        }
+
+        
+        else{
+            if($status)
+            {
+                
+                $sql=  "INSERT INTO donor (name ,email, type) values('$na','$em','$ty') " ;
+                if($conn->query($sql)){
+                    header('Location: thank.html');
+                }
+                else{
+                    echo "Error:".$sql."<br>".$conn->error;
+                }
+                $conn->close();
+            }
+            
+        }
+    }
+
+    ?>
+
+	<!-- main html -->
+	<div class="main-w3layouts wrapper">
+		<h1>Signup Here</h1>
+		<div class="main-agileinfo">
+			<div class="agileits-top">
+				<form action="#" method="post">
+                    <input class="text" type="text" name="name" placeholder="Name" required autofocus >
+                    <input class="text email" type="email" name="email" placeholder="Email" required  >
+                    <select name="type">
+                    <option value="A+">A+</option>
+                            <option value="A-">A-</option>
+                            <option value="B+">B+</option>
+                            <option value="B-">B-</option>
+                            <option value="O+">O+</option>
+                            <option value="O-">O-</option>
+                            <option value="AB+">AB+</option>
+                            <option value="AB-">AB-</option>
+                    </select>
+					<div class="wthree-text">
+						<div class="clear"> </div>
+					</div>
+					<input type="submit" value="SIGNUP">
+				</form>
+                
+                
+			</div>
+		</div>
+
+	</div>
+	<!-- //main -->
+</body>
+</html>
--- mailer.php
+++ mailer.php
@@ -1,50 +1,95 @@
-<?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
+<?php 
+session_start();
 
-    require 'PHPMailer/Exception.php';
-    require 'PHPMailer/PHPMailer.php';
-    require 'PHPMailer/SMTP.php';
+// if($_SESSION['loggedin']==true)
+// {
+//     $name= $_SESSION['userDetails']['name'];
+//     $address= $_SESSION['userDetails']['address']; 
+// }
     
-    // Load Composer's autoloader
-    // require 'vendor/autoload.php';
-    
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+//Database Connection
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="electrothon";
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+$type=$_POST['type'];
+
+if($conn-> connect_error)
+{
+    die("Connection failed".$conn-> connect_error);
+}
+else{
+    $sql="SELECT email FROM donor WHERE type='$type'";
+    $result=$conn->query($sql);
+    if($result->num_rows>0)
+        {
+            while($row=$result->fetch_assoc()){
+            $email=$row['email'];
+            sendMail($email);
+            }
+        }
+
+}
+
+
+//try
+function sendMail($email) {
     // Instantiation and passing `true` enables exceptions
     $mail = new PHPMailer(true);
-    
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'adityabisoi1999@gmail.com';                     // SMTP username
-        $mail->Password   = 'Aditya@123';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = 587;                                    // TCP port to connect to
-    
-        //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        // $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-        $mail->addAddress('burgerleo90@gmail.com');               // Name is optional
-        // $mail->addReplyTo('info@example.com', 'Information');
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('bcc@example.com');
-    
-        // Attachments
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+    $name= $_SESSION['userDetails']['name'];
+    $address= $_SESSION['userDetails']['email'];
+    $type=$_POST['type'];
+
+
+    //Fetch receiver's details
+
+    // $rname=$_POST['rname'];
+    // $number=$_POST['number'];
+    // $type=$_POST['type'];
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'adityabisoi1999@gmail.com';                     // SMTP username
+    $mail->Password   = 'Aditya@123';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption, `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('no_reply@gmail.com');
+    //$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress($email);               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Blood needed urgently !';
+    // $mail->Body    = $bgroup." blood needed urgently.Please contact ".$rname." at ".$number." as soon as possible.";
+    $mail->Body    =$type." blood needed ugently in ".$name." hospital at ".$address." .Please contact them as soon as possible.";
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    header('Location: test.php');
+}
+//  catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// }
 ?>
